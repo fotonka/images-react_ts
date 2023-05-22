275ba58002558b729358830de7cd5655be4879d4
Kirill Chaialo <99725412+fotonka@users.noreply.github.com>
2023-05-22 14:29:26 +0200

    css-correct

--- images/src/components/pages/main-page/gallery/Gallery.tsx
+++ images/src/components/pages/main-page/gallery/Gallery.tsx
@@ -52,40 +52,6 @@ const Gallery = () => {
             </div>
           </a>
         </li>
-
-        <li className="gallery-item">
-          <a className="gallery-link" href="https://google.com/">
-            <div className="gallery-image">
-              <img
-                className="gallery-picture"
-                src="https://images.pexels.com/photos/268533/pexels-photo-268533.jpeg?cs=srgb&dl=pexels-pixabay-268533.jpg&fm=jpg"
-                alt="pic"
-              />
-            </div>
-            <div className="gallery-control">
-              <button className="button button-add">1</button>
-              <button className="button button-add">2</button>
-              <button className="button button-add">3</button>
-            </div>
-          </a>
-        </li>
-
-        <li className="gallery-item">
-          <a className="gallery-link" href="https://google.com/">
-            <div className="gallery-image">
-              <img
-                className="gallery-picture"
-                src="https://images.pexels.com/photos/268533/pexels-photo-268533.jpeg?cs=srgb&dl=pexels-pixabay-268533.jpg&fm=jpg"
-                alt="pic"
-              />
-            </div>
-            <div className="gallery-control">
-              <button className="button button-add">1</button>
-              <button className="button button-add">2</button>
-              <button className="button button-add">3</button>
-            </div>
-          </a>
-        </li>
       </ul>
     </div>
   );
